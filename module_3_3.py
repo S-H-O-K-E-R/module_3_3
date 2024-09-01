0cd1bdedb6761e269342a25693433414aff7b07c
S-H-O-K-E-R <kolyan482650@gmail.com>
2024-09-01 14:44:32 +0500

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,14 +1,34 @@
-message = 'Это сообщение для проверки связи'
-recipient = 'vasyok1337@gmail.uk'
-sender = 'urban.teacher@mail.com'
-end_1 = recipient.endswith((".com", '.ru', '.net')) and sender.endswith((".com", '.ru', '.net'))
-find_1 = recipient.find('@') and sender.find('@')
-if end_1 == 0  or find_1 < 0:
-    print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-elif sender == recipient:
-    print("Нельзя отправить письмо самому себе!")
-elif sender == 'university.help@gmail.com'and recipient != 'university.help@gmail.com':
-    print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-else:
-    print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+def print_params(a = 1, b = 'строка', c = True):
+  if a == None and b == None and c == None:
+      print()
+  elif b == None and c == None:
+      print(a)
+  elif a == None and c == None:
+      print(b)
+  elif a == None and b == None:
+      print(c)
+  elif a == None:
+      print(b, c)
+  elif b == None:
+      print(a, c)
+  elif c == None:
+      print(a,b)
+  else:
+    print(a,b,c)
 
+print_params()
+print_params(a=None, b=None, c=None)
+print_params(a=True, b=None, c=12.3)
+print_params(a=None, b=10, c=None)
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [2, True, 'Hi']
+values_dict = {'a': 5, 'b': False, 'c': 'Bye'}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [25.5, False]
+print_params(5, *values_list_2)
